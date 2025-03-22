d0493b064df543a92b67b686ea26a717d4e6b01a
Гвоздев Кирилл Александрович <kirilljjjjj@mail.ru>
2025-03-22 18:41:24 +0300

    individualochka

--- LR2/Form1.cs
+++ LR2/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -21,18 +23,27 @@ namespace LR2
         {
             double x = double.Parse(textBox1.Text);
             double a = double.Parse(textBox2.Text);
-            if (x)
-            double b = 2 / x;
-            double c = Math.Sqrt(Math.Pow(a, 3) - Math.Pow(x, 2));
-            double d = b * c;
-            double s = Math.Pow(a, 2) / 2;
-            double r = Math.Asin(x / Math.Abs(a));
-            double g = s * r;
-            double F = d + g;
-            label4.Text = "Значение F в 10 с. и.: " + F.ToString();
-            int q = (int)Math.Floor(F);
-            string bF = Convert.ToString(q, 2);
-            label5.Text = "Значение F в 2 с. и.: " + bF.ToString();
+            double v = Math.Pow(a, 3) - Math.Pow(x, 2);
+            if (x != 0 && a != 0 && v >= 0)
+            {
+                double b = 2 / x;
+
+                double c = Math.Sqrt(v);
+                double d = b * c;
+                double s = Math.Pow(a, 2) / 2;
+                double r = Math.Asin(x / Math.Abs(a));
+                double g = s * r;
+                double F = d + g;
+                label4.Text = "Значение F в 10 с. и.: " + F.ToString();
+                int q = (int)Math.Floor(F);
+                string bF = Convert.ToString(q, 2);
+                label5.Text = "Значение F в 2 с. и.: " + bF.ToString();
+            }
+            else
+            {
+                label4.Text = "Невозможно посчитать";
+                label5.Text = "Невозможно посчитать";
+            }
         }
     }
 }
